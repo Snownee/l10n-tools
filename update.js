965d27a004e3b9d539cc4d0e2843a0890b09c372
Snownee <mjy523@126.com>
2019-07-03 03:58:57 +0800

    Support for JSON

--- update.js
+++ update.js
@@ -17,11 +17,29 @@ jQuery(document).ready( function($) {
   $("#update").click( function() {
     var _old = $("#old").val(),
         _new = $("#new").val(),
-        ks = listKeys(_new);
-    for ( i in ks ) {
-      var v = get( _old, ks[i] );
-      if (v) {
-        _new = set( _new, ks[i], v )
+        json = true;
+    try {
+      _old = JSON.parse(_old);
+      _new = JSON.parse(_new);
+    } catch (e) {
+      json = false;
+    }
+    if (json) {
+      for (var k of Object.keys(_new)) {
+        v = _old[k];
+        if (typeof(v) == 'string')
+        {
+          _new[k] = v;
+        }
+      }
+      _new = JSON.stringify(_new, null, 1);
+    } else {
+      var ks = listKeys(_new);
+      for ( i in ks ) {
+        var v = get( _old, ks[i] );
+        if (v) {
+          _new = set( _new, ks[i], v )
+        }
       }
     }
     $("#new").val(_new)
